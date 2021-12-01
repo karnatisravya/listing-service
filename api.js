2d563206dd1f4c2b7c21eb797fa42c984d183558
Sravya Karnati <varmamaryala@Phaneendras-iMac.local>
2021-11-30 21:32:10 -0800

    first commit

---
+++ api.js
@@ -0,0 +1,171 @@
+const db = require("./db");
+const {
+  GetItemCommand,
+  PutItemCommand,
+  DeleteItemCommand,
+  ScanCommand,
+  UpdateItemCommand,
+} = require("@aws-sdk/client-dynamodb");
+const { marshall, unmarshall } = require("@aws-sdk/util-dynamodb");
+
+const getListing = async (event) => {
+  const response = { statusCode: 200 };
+
+  try {
+    const params = {
+      TableName: process.env.DYNAMODB_TABLE_NAME,
+      Key: marshall({ postId: event.pathParameters.listingId }),
+    };
+    const { Item } = await db.send(new GetItemCommand(params));
+
+    console.log({ Item });
+    response.body = JSON.stringify({
+      message: "Successfully retrieved listing.",
+      data: (Item) ? unmarshall(Item) : {},
+      rawData: Item,
+    });
+  } catch (e) {
+    console.error(e);
+    response.statusCode = 500;
+    response.body = JSON.stringify({
+      message: "Failed to get listing.",
+      errorMsg: e.message,
+      errorStack: e.stack,
+    });
+  }
+
+  return response;
+};
+
+const createListing = async (event) => {
+  const response = { statusCode: 200 };
+
+  try {
+    const body = JSON.parse(event.body);
+    const params = {
+      TableName: process.env.DYNAMODB_TABLE_NAME,
+      Item: marshall(body || {}),
+    };
+    const createResult = await db.send(new PutItemCommand(params));
+
+    response.body = JSON.stringify({
+      message: "Successfully created listing.",
+      createResult,
+    });
+  } catch (e) {
+    console.error(e);
+    response.statusCode = 500;
+    response.body = JSON.stringify({
+      message: "Failed to create listing.",
+      errorMsg: e.message,
+      errorStack: e.stack,
+    });
+  }
+
+  return response;
+};
+
+const updateListing = async (event) => {
+  const response = { statusCode: 200 };
+
+  try {
+    const body = JSON.parse(event.body);
+    const objKeys = Object.keys(body);
+    const params = {
+      TableName: process.env.DYNAMODB_TABLE_NAME,
+      Key: marshall({ postId: event.pathParameters.listingId }),
+      UpdateExpression: `SET ${objKeys.map((_, index) => `#key${index} = :value${index}`).join(", ")}`,
+      ExpressionAttributeNames: objKeys.reduce((acc, key, index) => ({
+        ...acc,
+        [`#key${index}`]: key,
+      }), {}),
+      ExpressionAttributeValues: marshall(objKeys.reduce((acc, key, index) => ({
+        ...acc,
+        [`:value${index}`]: body[key],
+      }), {})),
+    };
+    const updateResult = await db.send(new UpdateItemCommand(params));
+
+    response.body = JSON.stringify({
+      message: "Successfully updated listing.",
+      updateResult,
+    });
+  } catch (e) {
+    console.error(e);
+    response.statusCode = 500;
+    response.body = JSON.stringify({
+      message: "Failed to update listing.",
+      errorMsg: e.message,
+      errorStack: e.stack,
+    });
+  }
+
+  return response;
+};
+
+const deleteListing = async (event) => {
+  const response = { statusCode: 200 };
+
+  try {
+    const params = {
+      TableName: process.env.DYNAMODB_TABLE_NAME,
+      Key: marshall({ postId: event.pathParameters.listingId }),
+    };
+    const deleteResult = await db.send(new DeleteItemCommand(params));
+
+    response.body = JSON.stringify({
+      message: "Successfully deleted listing.",
+      deleteResult,
+    });
+  } catch (e) {
+    console.error(e);
+    response.statusCode = 500;
+    response.body = JSON.stringify({
+      message: "Failed to delete listing.",
+      errorMsg: e.message,
+      errorStack: e.stack,
+    });
+  }
+
+  return response;
+};
+
+const getAllListings = async (event) => {
+  const response = { statusCode: 200 };
+
+  try {
+    const params = {
+      TableName: process.env.DYNAMODB_TABLE_NAME,
+      FilterExpression: "category-id = :category-id",
+      // Define the expression attribute value, which are substitutes for the values you want to compare.
+      ExpressionAttributeValues: {
+        ":category-id": {N: event.pathParameters.categoryId},
+      },
+    };
+    const { Items } = await db.send(new ScanCommand({ TableName: process.env.DYNAMODB_TABLE_NAME }));
+
+    response.body = JSON.stringify({
+      message: "Successfully retrieved all listings for a category.",
+      data: Items.map((item) => unmarshall(item)),
+      Items,
+    });
+  } catch (e) {
+    console.error(e);
+    response.statusCode = 500;
+    response.body = JSON.stringify({
+      message: "Failed to retrieve listings.",
+      errorMsg: e.message,
+      errorStack: e.stack,
+    });
+  }
+
+  return response;
+};
+
+module.exports = {
+  getListing,
+  createListing,
+  updateListing,
+  deleteListing,
+  getAllListings,
+};
